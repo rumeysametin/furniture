96c0c6f3a020233e307db415f378711e74e6026c
rumeysametin <rumeysametin1166@gmail.com>
2022-02-19 19:41:25 +0300

    database models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Productcode)
+admin.site.register(Color)
+admin.site.register(Material)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "cod", "brand", "color", "material", "price", "status")
+    list_editable = ("status",)
+    
+admin.site.register(Product, ProductAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="cat_imgs/")
+
+    def __str__(self):
+        return self.title
+    
+class Productcode(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Brand(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Color(models.Model):
+    title = models.CharField(max_length=100)
+    color_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Material(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="product_imgs/")
+    detail = models.TextField()
+    price = models.PositiveIntegerField()
+    category =models.ForeignKey(Category, on_delete=models.CASCADE)
+    cod =models.ForeignKey(Productcode, on_delete=models.CASCADE)
+    brand =models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color =models.ForeignKey(Color, on_delete=models.CASCADE)
+    material =models.ForeignKey(Material, on_delete=models.CASCADE)
+    status =models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
